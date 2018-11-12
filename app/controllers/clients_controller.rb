26d449bb970b679570e910e4833a2cb4a7f4356e
Abril Davila <abrildavas@gmail.com>
2018-11-12 20:51:06 -0300

    RE:correccion Vistas home - iniciar sesion

---
+++ app/controllers/clients_controller.rb
@@ -0,0 +1,23 @@
+class ClientsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def sesionIniciada
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,25 @@
 Rails.application.routes.draw do
 
-
   resources :administrators
   resources :residences
   
-  
+
   root 'administrators#home'
+
+  get 'clients/index'
+
+  get 'clients/show'
+
+  get 'clients/new'
+
+  get 'clients/edit'
+
+  get 'clients/create'
+
+  get 'clients/destroy'
+
+  get 'clients/sesionIniciada', to: 'clients#sesionIniciada'
+  
 
   get  'iniciarSesion',to:'administrators#iniciarSesion'
 
@@ -13,7 +27,5 @@ Rails.application.routes.draw do
 
   get 'default', to: 'administrators#default'
 
- 
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
