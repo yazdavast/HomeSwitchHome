f3b40001069f2896c174b3e4f008d83d3a8bb272
Yazmin <yazdavast@gmail.com>
2018-11-13 10:44:55 -0300

    vistas y seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,7 @@ Administrator.find_or_create_by(nombre: "Yazmin", apellido: "Davila", nomUsuario
 
 
 
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
