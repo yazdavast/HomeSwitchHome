7a2fe852dbf8fe8607dc295811e642aa99e4f968
Yazmin <yazdavast@gmail.com>
2018-11-12 21:07:24 -0300

    .

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,14 @@ Rails.application.routes.draw do
   resources :administrators
   resources :residences
   
+  
+  root 'administrators#home'
 
-  get  '/iniciarSesion', to:'administrators#iniciarSesion'
+  get  'iniciarSesion',to:'administrators#iniciarSesion'
 
+  get 'validarLogIn',to:'administrators#validarLogIn'
 
- 
+  get 'default', to: 'administrators#default'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
